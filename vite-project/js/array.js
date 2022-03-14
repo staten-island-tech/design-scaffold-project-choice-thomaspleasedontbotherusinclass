323e9e4c7cdf6aa33d49d125426dec2a852e80d1
JamesonGreene <jmesgian@gmail.com>
2022-03-14 10:56:52 -0400

    oops not finished lol xd :tf:

---
+++ vite-project/js/array.js
@@ -0,0 +1,10 @@
+const MenuItems = [
+  {
+    item: "Vegan Burger",
+    cost: 12,
+    description:
+      "Enjoy the scale Crushing taste of our famous bypass burger without the consequences of eating meat. Our burgers are made of 100% impossible meat and cooked on the grill using corn oil in order to replicate that classic heart attack grill taste. Served with cheese lettuce and onions with extra patties and bacon optional.",
+    type: "Entree",
+  },
+];
+export { MenuItems };
