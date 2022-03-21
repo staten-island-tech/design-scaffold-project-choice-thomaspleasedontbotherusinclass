93c2c0659f7a80422303cd9f7cc3c62e0907bc0a
JamesonGreene <jmesgian@gmail.com>
2022-03-21 10:41:44 -0400

    a

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -53,3 +53,12 @@ Entrees.forEach((Entrees) => {
 //     />`
 //   );
 // });
+DOMSelectors.themebtn.addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
